62d944aa6b75ab3229fa03826db8c204f406e85a
Nikolay Novik <nickolainovik@gmail.com>
2019-01-21 22:57:06 +0200

    make main code working

---
+++ mung/approx.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils import shuffle
+from keras.layers import Dense
+from keras.layers.normalization import BatchNormalization
+
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.wrappers.scikit_learn import KerasRegressor
+
+from .generator import Munge
+
+
+def make_model(
+        input_size=None,
+        activation='relu',
+        loss='mean_squared_error',
+        optimizer_params=None,
+        hidden_layer_size=None):
+
+    hidden_size = hidden_layer_size or int(input_size * 0.75)
+    model = Sequential()
+    model.add(Dense(
+        hidden_size, input_dim=input_size,
+        kernel_initializer='normal',
+        activation=activation,
+        use_bias=False,
+    ))
+    model.add(BatchNormalization())
+    model.add(Dense(
+        hidden_size,
+        kernel_initializer='normal',
+        activation=activation,
+        use_bias=False,
+    ))
+    model.add(BatchNormalization())
+    model.add(Dense(
+        hidden_size,
+        kernel_initializer='normal',
+        activation=activation,
+        use_bias=False,
+    ))
+    model.add(BatchNormalization())
+    model.add(
+        Dense(1, kernel_initializer='normal')
+    )
+    # Compile model
+    optimizer_params = optimizer_params or {}
+    adam = Adam(**optimizer_params)
+    model.compile(loss=loss, optimizer=adam)
+    model.summary()
+    return model
+
+
+class KerasRegressionApprox(BaseEstimator, RegressorMixin):
+
+    def __init__(self,
+                 clf=None,
+                 epochs=128,
+                 batch_size=8,
+                 random_state=None,
+                 activation='relu',
+                 loss='mean_squared_error',
+                 optimizer_params=None,
+                 sample_multiplier=10,
+                 hidden_layer_size=None,
+                 p=0.5,
+                 s=2):
+
+        self.clf = clf
+        self.model = None
+        self.random_state = random_state
+        self.sample_multiplier = sample_multiplier
+        self.p = p
+        self.s = s
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.hidden_layer_size = hidden_layer_size
+
+    def _new_data(self, X_train, y_train):
+        m = Munge(p=self.p, s=self.s, seed=self.random_state)
+        m.fit(X_train)
+
+        X_train_new = m.sample(X_train.shape[0] * self.sample_multiplier)
+        y_train_new = self.clf.predict(X_train_new)
+        X = np.concatenate((X_train_new, X_train), axis=0)
+        y = np.concatenate((y_train_new, y_train), axis=0)
+        X, y = shuffle(X, y, random_state=self.random_state)
+        return X, y
+
+    def fit(self, X, y, **kw):
+        X_train, y_train = self._new_data(X, y)
+        input_size = X.shape[1]
+
+        self.model = KerasRegressor(
+            build_fn=make_model,
+            input_size=input_size,
+            epochs=self.epochs,
+            batch_size=self.batch_size,
+            hidden_layer_size=self.hidden_layer_size,
+            verbose=1)
+        self.model.fit(X_train, y_train, **kw)
+        return self
+
+    def predict(self, y, **kw):
+        return self.model.predict(y, **kw)
---
+++ mung/generator.py
@@ -0,0 +1,114 @@
+import numpy as np
+from sklearn.metrics import pairwise_distances_chunked
+from sklearn.metrics.pairwise import check_pairwise_arrays
+
+
+def argmink(arr, k=1):
+    return np.argpartition(arr, k, axis=0)[k]
+
+
+def _argmin_min_reduce(dist, start):
+    indices = np.apply_along_axis(argmink, 1, dist)
+    values = dist[np.arange(dist.shape[0]), indices]
+    return indices, values
+
+
+def pairwise_distances(X, Y, axis=1, metric="euclidean", metric_kwargs=None):
+    X, Y = check_pairwise_arrays(X, Y)
+
+    if metric_kwargs is None:
+        metric_kwargs = {}
+
+    if axis == 0:
+        X, Y = Y, X
+
+    indices, values = zip(*pairwise_distances_chunked(
+        X, Y, reduce_func=_argmin_min_reduce, metric=metric,
+        **metric_kwargs))
+    indices = np.concatenate(indices)
+    values = np.concatenate(values)
+    return indices, values
+
+
+class Munge:
+
+    def __init__(self, *, p=0.8, s=1, seed=None):
+        self.p: float = p
+        self.s: float = s
+
+        self.X = None
+        self.categorical_features = []
+        self.seed = seed
+
+    def fit(self, X, categorical_features=None):
+        """Estimate model parameters with the Munge algorithm.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+
+        Returns
+        -------
+        self
+        """
+        if categorical_features:
+            self.categorical_features = categorical_features
+        self.X = X
+        return self
+
+    def sample(self, n_samples=1):
+        """Generate random samples from the fitted Gaussian distribution.
+
+        Parameters
+        ----------
+        n_samples : int, optional
+            Number of samples to generate. Defaults to 1.
+        Returns
+        -------
+        X : array, shape (n_samples, n_features)
+            Randomly generated sample
+        return self.X
+        """
+        rows = self.X.shape[0]
+        replace = rows < n_samples
+        num_features = self.X.shape[1]
+
+        sample_rng = np.random.RandomState(new_seed(self.seed, 0))
+        feature_rng = np.random.RandomState(new_seed(self.seed, 1))
+        value_rng = np.random.RandomState(new_seed(self.seed, 2))
+
+        row_ids = sample_rng.choice(rows, n_samples, replace=replace)
+
+        sampled_data = self.X[row_ids, :]
+        argmin, _ = pairwise_distances(
+            sampled_data, self.X, metric='euclidean', axis=1)
+
+        for i in range(n_samples):
+            nearest_idx = argmin[i]
+            nearest = self.X[nearest_idx]
+            for feature_idx in range(num_features):
+                if feature_rng.rand() < self.p:
+                    if feature_idx not in self.categorical_features:
+                        old = sampled_data[i, feature_idx]
+                        new = nearest[feature_idx]
+                        sd = np.abs(old - new) / self.s
+                        sampled_data[i, feature_idx] = value_rng.normal(
+                            new, sd)
+                    else:
+                        sampled_data[i, feature_idx] = new
+
+        return sampled_data
+
+    def _nearest_sample(self, instance):
+        x = self.X
+        distances = np.linalg.norm(x - instance)
+        idx = np.argmin(distances, axis=None)
+        return self.X[idx, :]
+
+
+def new_seed(seed, increment):
+    if seed is None:
+        return None
+    return seed + increment
