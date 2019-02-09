2faf644732f3db6bf3b6bdee718d6c760949b810
Nikolay Novik <nickolainovik@gmail.com>
2019-02-09 22:09:42 +0200

    add test case for classification

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from mung import Munge, EncodingType
-from mung.approx import KerasRegressionApprox
+from mung.approx import KerasRegressionApprox, KerasClassifictionApprox
 from mung.helpers import ScaledFreqEncoder
 from mung.utils import advesarial_validator
 
 from sklearn import datasets
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error
+import lightgbm as lgb
+from sklearn.metrics import mean_squared_error, log_loss
 from sklearn.preprocessing import MinMaxScaler
 
 import pytest
@@ -131,6 +132,26 @@ def test_keras_regressor(boston, seed):
     keras_approx_mse = mean_squared_error(y_test, kr.predict(X_test))
     gbr_mse = mean_squared_error(y_test, gbr.predict(X_test))
     assert gbr_mse < keras_approx_mse
+
+
+def test_keras_classificator(adult, seed):
+    X_train, X_test, y_train, y_test = adult
+    categorical_features = list(range(5, X_train.shape[1]))
+
+    clf = lgb.LGBMClassifier(
+        objective='binary',
+        learning_rate=0.01,
+        metric='logloss',
+        categorical_feature=categorical_features)
+    clf.fit(X_train, y_train)
+
+    kc = KerasClassifictionApprox(
+        clf, sample_multiplier=2, epochs=10, batch_size=2000)
+    kc.fit(X_train, y_train)
+
+    keras_approx_logloss = log_loss(y_test, kc.predict(X_test))
+    clf_logloss = log_loss(y_test, clf.predict(X_test))
+    assert clf_logloss < keras_approx_logloss
 
 
 def test_freq(adult, seed):
