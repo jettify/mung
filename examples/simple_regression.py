a0bd663b94ffdd974d243242c260c840ea3310ff
Nikolay Novik <nickolainovik@gmail.com>
2019-03-24 18:41:38 +0200

    move to function

--- examples/simple_regression.py
+++ examples/simple_regression.py
@@ -15,13 +15,15 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
 
-# make results reprocducable, Kerad does not support seeds
-# explicitly properly
-np.random.seed(42)
-session_conf = tf.ConfigProto(intra_op_parallelism_threads=1,
-                              inter_op_parallelism_threads=1)
-sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
-K.set_session(sess)
+
+def set_global_seeds(seed=42):
+    # make results reproducible, Keras does not support seeds
+    # explicitly properly
+    np.random.seed(seed)
+    session_conf = tf.ConfigProto(intra_op_parallelism_threads=1,
+                                  inter_op_parallelism_threads=1)
+    sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+    K.set_session(sess)
 
 
 def fit_lgbm_model(X_train, y_train, X_test, y_test, seed=None):
@@ -50,6 +52,7 @@ def make_model(
         loss='mean_squared_error',
         optimizer_params=None,
         hidden_layer_size=None,
+        lr=0.0005,
         seed=42):
 
     hidden_size = hidden_layer_size or int(input_size * 0.75)
@@ -73,7 +76,7 @@ def make_model(
 
     model.add(Dense(1, kernel_initializer='uniform'))
 
-    opt = SGD(lr=0.0005, momentum=0.0, decay=0.0, nesterov=True)
+    opt = SGD(lr=lr, momentum=0.0, decay=0.0, nesterov=True)
     model.compile(loss=loss, optimizer=opt)
     model.summary()
     return model
@@ -98,15 +101,23 @@ def fit_approx_keras_model(X_train, y_train, X_test, y_test, clf, seed=None):
     kr = KerasRegressor(
         build_fn=make_model,
         input_size=X_train_new.shape[1],
-        epochs=256,
+        epochs=300,
         batch_size=200,
         hidden_layer_size=12,
+        lr=0.0005
     )
+    # fit synthetic data
     kr.fit(X_train_new, y_train_new)
+    keras_mse = mean_squared_error(y_test, kr.predict(X_test))
+    print(f'Without Real Keras with Approximation MSE: {keras_mse}')
+
+
+    # fit real data data
+    K.set_value(kr.model.optimizer.lr, 0.0002)
     kr.fit(X_train, y_train)
 
     keras_mse = mean_squared_error(y_test, kr.predict(X_test))
-    print(f'Keras with Approximation MSE: {keras_mse}')
+    print(f'With Real Keras with Approximation MSE: {keras_mse}')
     return kr
 
 
@@ -122,6 +133,7 @@ def fit_keras_model(X_train, y_train, X_test, y_test, seed=None):
         epochs=256,
         batch_size=200,
         hidden_layer_size=12,
+        lr=0.0005
     )
     kr.fit(X_train, y_train)
 
@@ -132,11 +144,13 @@ def fit_keras_model(X_train, y_train, X_test, y_test, seed=None):
 
 def main():
     seed = 42
+    set_global_seeds(seed=seed)
     X_train, y_train, X_test, y_test = load_boston(seed=seed)
     clf = fit_lgbm_model(X_train, y_train, X_test, y_test, seed=seed)
     kr = fit_keras_model(X_train, y_train, X_test, y_test, seed=seed)
-    kra = fit_approx_keras_model(X_train, y_train, X_test, y_test, clf, seed=seed)
-    assert (clf, kra, kr)
+    kra = fit_approx_keras_model(
+        X_train, y_train, X_test, y_test, clf, seed=seed)
+    (clf, kra, kr)
 
 
 if __name__ == '__main__':
